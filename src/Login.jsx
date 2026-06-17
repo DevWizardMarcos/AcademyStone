2f14a6b16002dc8c6b228fec6612a2e44ac75fe5
DevWizardMarcos <mp18887@gmail.com>
2026-06-17 18:33:38 -0300

    feath:começando a tela de login

---
+++ src/Login.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import "./css/Login.css";
+
+function App() {
+  return (
+    <>
+      <section id="formulario">
+        <div className="sessaoFormulario">
+          <form action="" method="get">
+            <h1>Login</h1>
+            <label htmlFor="">Digite Seu Email</label>
+            <input type="email" name="" id="" />
+            <label htmlFor="">Digite Sua Senha</label>
+            <input type="email" name="" id="" />
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
