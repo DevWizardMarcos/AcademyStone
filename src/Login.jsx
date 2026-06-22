9a4cbf7a08711d4382c996936a9a148e47a658da
DevWizardMarcos <mp718887@gmail.com>
2026-06-22 18:54:44 -0300

    feacth estilizando o menu

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
-
 import "./css/Login.css";
 
-function App() {
+function Login() {
   return (
     <>
       <section id="formulario">
         <div className="sessaoFormulario">
           <form action="" method="get">
-            <h1>Login</h1>
-            <label htmlFor="">Digite Seu Email</label>
-            <input type="email" name="" id="" />
-            <label htmlFor="">Digite Sua Senha</label>
-            <input type="email" name="" id="" />
+            <h1>Instrutor</h1>
+            <h2>Stone</h2>
+            <span>Card Academy</span>
+            <input
+              type="email"
+              className="input-email"
+              placeholder=" 👤 Digite seu email"
+            />
+            <input
+              type="password"
+              className="input-senha"
+              placeholder="🔒 Digite sua senha "
+            />
+            <div className="lembrar">
+              <input type="checkbox" name="" id="lembrar"/>
+              <span className="lembrese">Lembre de mim</span>
+              <span>
+                <a href="">Esqueci minha senha</a>
+              </span>
+            </div>
+
+            <button id="Entrar">Entrar</button>
+            <span>ou</span>
+            <button id="Google">Econtar Conta Google</button>
+            <span>
+              Ainda não tem Conta ? <a href="">cadastrese</a>
+            </span>
           </form>
         </div>
       </section>
@@ -20,4 +40,4 @@ function App() {
   );
 }
 
-export default App;
+export {Login};
